73f9c37e7e126e4e49ce72726777d1e55be9961d
Bartek Pankowski <bpankowski0@gmail.com>
2017-12-15 23:03:24 +0100

    23:03

--- Manager/src/app/navigation/navigation.component.ts
+++ Manager/src/app/navigation/navigation.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  customers = true;
+  orders = false
+  about = false;
+  login = false;
 
   ngOnInit() {
+  }
+
+  changeClass(event):void {
+    if(event.target.id === 'customers'){
+      this.customers = true;
+      this.orders = false
+      this.about = false;
+      this.login = false;
+    }else if(event.target.id === 'orders'){
+      this.customers = false;
+      this.orders = true;
+      this.about = false;
+      this.login = false;
+    }else if(event.target.id === 'about'){
+      this.customers = false;
+      this.orders = false;
+      this.about = true;
+      this.login = false;
+    }else{
+      this.customers = false;
+      this.orders = false;
+      this.about = false;
+      this.login = true;
+    }
   }
 
 }

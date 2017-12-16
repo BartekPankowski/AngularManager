364b3f8a840ccc8930a24d10660748f18d5fdd47
Bartek Pankowski <bpankowski0@gmail.com>
2017-12-17 00:04:14 +0100

    00:04

--- Manager/src/app/app-routing.module.ts
+++ Manager/src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 import { CustomersComponent } from './customers/customers.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { OrdersComponent } from './orders/orders.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { patch } from 'webdriver-js-extender';
+import { CardViewComponent } from './card-view/card-view.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/customers', pathMatch: 'full' },
-  {path: 'customers', component: CustomersComponent},
+  {path: 'customers', component: CustomersComponent, children: [
+    { path: '', redirectTo: '/customers/card-view', pathMatch: 'full' },
+    {path: 'card-view', component: CardViewComponent},
+    {path: 'list-view', component: ListViewComponent},
+    {path: 'map-view', component: MapViewComponent},
+    {path: 'new-customer', component: NewCustomerComponent}
+  ]},
   {path: 'orders', component: OrdersComponent},
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent}
--- Manager/src/app/customers/customers.component.ts
+++ Manager/src/app/customers/customers.component.ts
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+    
   }
 
 }
